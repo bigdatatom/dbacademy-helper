1d2b22bd3d51a1b27e6386455023a6f12ebc6214
Jacob D. Parr <jacob.parr@databricks.com>
2022-08-23 16:33:03 -0500

    Update databases_helper.py

--- src/dbacademy_helper/databases_helper.py
+++ src/dbacademy_helper/databases_helper.py
@@ -18,11 +18,11 @@ class DatabasesHelper:
         self.workspaces._usernames = None
         self.workspaces._existing_databases = None
 
-    def create_database_for(self, username: str, drop_existing: bool, post_create_init: Callable[[str], None]):
+    def create_database_for(self, username: str, drop_existing: bool, post_create_init: Callable[[str], None] = None):
         db_name = self.da.to_database_name(username=username, course_code=self.da.course_code)
         db_path = f"dbfs:/mnt/dbacademy-users/{username}/{self.da.course_name}/database.db"
 
-        if db_name in self.da.workspace.databases and drop_existing:
+        if db_name in self.da.workspace.existing_databases and drop_existing:
             dbgems.get_spark_session().sql(f"DROP DATABASE {db_name} CASCADE;")
 
         # Create the database
