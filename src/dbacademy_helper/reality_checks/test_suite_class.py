51f037af09120cb87adcf2b3ff2d69f0fe1264b3
SireInsectus <jacob.parr@databricks.com>
2022-09-19 21:27:51 -0500

    Update test_suite_class.py

--- src/dbacademy_helper/reality_checks/test_suite_class.py
+++ src/dbacademy_helper/reality_checks/test_suite_class.py
@@ -26,7 +26,7 @@ class TestSuite(object):
         test_results = list()
 
         for test in self.test_cases:
-            print(f"Test #{test.test_case_id}: test.depends_on")
+            print(f"Test #{test.test_case_id}: {test.depends_on}")
 
             skip = any(test_id in failed_tests for test_id in test.depends_on)
             result = TestResult(test, skip)
