5ea51230c5746c961de9f9f75fa9d2714b26a4f6
SireInsectus <jacob.parr@databricks.com>
2022-09-22 13:00:22 -0500

    Update __init__.py

--- src/dbacademy_helper/__init__.py
+++ src/dbacademy_helper/__init__.py
@@ -86,7 +86,17 @@ class DBAcademyHelper:
         self.username = dbgems.sql("SELECT current_user()").first()[0]
 
         # This is the location in our Azure data repository of the datasets for this lesson
-        self.data_source_uri = f"wasbs://courseware@dbacademy.blob.core.windows.net/{self.data_source_name}/{self.data_source_version}"
+        self.staging_source_uri = f"wasbs://courseware@dbacademy.blob.core.windows.net/_dbacademy-datasets-staging/{self.data_source_name}/{self.data_source_version}"
+        try:
+            files = dbgems.get_dbutils().ls(self.staging_source_uri)
+            if len(files) > 0:
+                self.data_source_uri = self.staging_source_uri
+                print("*"*80)
+                print(f"* Found Staging-Datasets for {self.data_source_name}/{self.data_source_version}")
+                print(f"* Using alternate install location.")
+                print("*"*80)
+        except:
+            self.data_source_uri = f"wasbs://courseware@dbacademy.blob.core.windows.net/{self.data_source_name}/{self.data_source_version}"
 
         ###########################################################################################
         # The follow section focuses on the schema and catalog names.
