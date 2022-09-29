69f71232b99e679fd6a9ec57b245b182166d2bd2
SireInsectus <jacob.parr@databricks.com>
2022-09-29 00:04:00 -0500

    Update __init__.py

--- src/dbacademy_helper/__init__.py
+++ src/dbacademy_helper/__init__.py
@@ -1,8 +1,8 @@
-try: import dbacademy_gems.dbgems
-except ImportError: raise Exception("The runtime dependency dbgems was not found. Please install https://github.com/databricks-academy/dbacademy-gems")
+try: from dbacademy_gems import dbgems
+except ImportError as e: raise Exception("The runtime dependency dbgems was not found. Please install https://github.com/databricks-academy/dbacademy-gems") from e
 
-try: import dbacademy.dbrest
-except ImportError: raise Exception("The runtime dependency dbrest was not found. Please install https://github.com/databricks-academy/dbacademy-rest")
+try: from dbacademy import dbrest
+except ImportError as e: raise Exception("The runtime dependency dbrest was not found. Please install https://github.com/databricks-academy/dbacademy-rest") from e
 
 from .dbacademy_helper_class import DBAcademyHelper
 from .paths_class import Paths
