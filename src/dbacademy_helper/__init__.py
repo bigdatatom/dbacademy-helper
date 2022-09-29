c93a12f5c627b87974959ef772b15dd29ec7bcdf
SireInsectus <jacob.parr@databricks.com>
2022-09-29 00:30:54 -0500

    Update __init__.py

--- src/dbacademy_helper/__init__.py
+++ src/dbacademy_helper/__init__.py
@@ -5,4 +5,5 @@ try: from dbacademy import dbrest
 except ImportError as e: raise Exception("The runtime dependency dbrest was not found. Please install https://github.com/databricks-academy/dbacademy-rest") from e
 
 from .dbacademy_helper_class import DBAcademyHelper
+from .env_config_class import EnvConfig
 from .paths_class import Paths
