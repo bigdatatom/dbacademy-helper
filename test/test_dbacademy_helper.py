9f8dd9c60e5eb8f8cc08e262d725c92a683bbff2
SireInsectus <jacob.parr@databricks.com>
2022-09-29 00:04:53 -0500

    Update test_dbacademy_helper.py

--- test/test_dbacademy_helper.py
+++ test/test_dbacademy_helper.py
@@ -3,23 +3,18 @@ from dbacademy_helper import DBAcademyHelper
 
 class MyTestCase(unittest.TestCase):
 
-    HELPER = DBAcademyHelper(course_code="ut",
-                             course_name="Unit Test",
-                             data_source_name="unit-test",
-                             data_source_version="unit-test",
-                             install_min_time="3 min",
-                             install_max_time="9 min",
-                             enable_streaming_support=False,
-                             remote_files=["/README.md"],
-                             lesson="Random Lesson",
-                             asynchronous=True)
+    def test_to_username_hash(self):
+        username = "mickey.mouse@disney.com"
+        da_name, da_hash = DBAcademyHelper.to_username_hash(username, "ex")
 
-    def test_to_schema_name(self):
-        username = "john.doe@example.com"
-        course_code = self.HELPER.course_code
-        db_name = DBAcademyHelper.to_schema_name(username, course_code)
+        self.assertEqual("mickey.mouse", da_name)
+        self.assertEqual(9984, da_hash)
 
-        self.assertEqual("adf", db_name)
+        username = "donald.duck@disney.com"
+        da_name, da_hash = DBAcademyHelper.to_username_hash(username, "tst")
+
+        self.assertEqual("donald.duck", da_name)
+        self.assertEqual(8192, da_hash)
 
 
 if __name__ == '__main__':
